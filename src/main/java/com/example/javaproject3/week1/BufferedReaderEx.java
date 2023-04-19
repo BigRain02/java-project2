7fedd29e72d4dc1afc4a90a51c1da90caf9762bd
zxfg223 <zxfg223@gmail.com>
2023-04-19 15:43:21 +0900

    feat: codeup1001 - codeup1001번 문제 코드

--- src/main/java/com/example/javaproject3/week1/BufferedReaderEx.java
+++ src/main/java/com/example/javaproject3/week1/BufferedReaderEx.java
@@ -14,4 +14,10 @@ public class BufferedReaderEx {
 
 
     }
+
+    public void plusTwoNumbers() throws IOException{
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is); // 카트 가지고 오기
+        System.out.println(br.readLine() + br.readLine());
+    }
 }
---
+++ src/main/java/com/example/javaproject3/week1/ScannerExTest.java
@@ -0,0 +1,9 @@
+package com.example.javaproject3.week1;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+        ScannerEx se = new ScannerEx();
+        se.readTwoNumbersAndPlus();
+
+    }
+}
