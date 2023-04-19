0a5ad893ccb6c90755851f4c54ec3d1bbf3e048f
zxfg223 <zxfg223@gmail.com>
2023-04-19 13:13:12 +0900

    feat: byeClass추가

---
+++ src/main/java/com/example/javaproject3/byeTestTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject3;
+
+public class byeTestTest {
+    public static void main(String[] args) {
+        byeClass byeClass = new byeClass();
+        byeClass.print();
+    }
+}
