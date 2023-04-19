06cfa57367f5448cab2585487ffb1f4a11d41eca
zxfg223 <zxfg223@gmail.com>
2023-04-19 15:49:58 +0900

    feat: codeup1002~10005 - codeup1002~1005번 문제 코드

--- src/main/java/com/example/javaproject3/week1/ScannerEx.java
+++ src/main/java/com/example/javaproject3/week1/ScannerEx.java
@@ -2,9 +2,7 @@ package com.example.javaproject3.week1;
 import java.util.Scanner;
 
 public class ScannerEx {
-    public void plusTwoNumbers(int val1, int val2) {
 
-    }
 
     public void readTwoNumbersAndPlus() {
         // 숫자 2개 입력 받기
