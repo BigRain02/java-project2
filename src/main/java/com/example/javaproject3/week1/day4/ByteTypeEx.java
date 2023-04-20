69abe5f85ba5a1af5f546541e62ac41961807b77
zxfg223 <zxfg223@gmail.com>
2023-04-20 14:36:06 +0900

    feat: 변수 활용 -ScannerFormating

---
+++ src/main/java/com/example/javaproject3/week1/day4/ByteTypeEx.java
@@ -0,0 +1,17 @@
+package com.example.javaproject3.week1.day4;
+
+public class ByteTypeEx {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = -128;
+        int byte3 = 129;
+        int byte4 = 128;
+        System.out.println(byte1);
+        System.out.println(byte2);
+        System.out.println(byte3);
+        System.out.println(byte4);
+
+
+
+    }
+}
