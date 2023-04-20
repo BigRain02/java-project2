1d9964ea9d74440d3b8a7f411f3371cb48929665
zxfg223 <zxfg223@gmail.com>
2023-04-20 11:48:20 +0900

    feat: print()활용 -print()이용하여 날짜출력

---
+++ src/main/java/com/example/javaproject3/week1/day4/PrinfDate.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week1.day4;
+
+public class PrinfDate {
+    public static void main(String[] args) {
+        System.out.printf("%d\n",4);
+        System.out.printf("%02d\n",4);
+        System.out.printf("%02d\n",11);
+        // 2023-04-20
+        System.out.printf("%d-%02d-%02d",2023,04,20);
+    }
+}
