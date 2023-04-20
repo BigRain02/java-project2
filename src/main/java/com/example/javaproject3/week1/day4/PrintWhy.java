152faa83d6ce290d544c1127ec8ffb9eaf78fc06
zxfg223 <zxfg223@gmail.com>
2023-04-20 11:18:49 +0900

    feat: print()활용 -print()이용하여 포맷팅

---
+++ src/main/java/com/example/javaproject3/week1/day4/PrintWhy.java
@@ -0,0 +1,11 @@
+package com.example.javaproject3.week1.day4;
+
+public class PrintWhy {
+    static void print(){
+        System.out.println("Why");
+    }
+
+    public static void main(String[] args) {
+        print();
+    }
+}
