b2c0da046341dc45d1a4a8e055ebde18aee09c5f
zxfg223 <zxfg223@gmail.com>
2023-04-20 15:20:56 +0900

    feat: 변수 선언 -new instance

---
+++ src/main/java/com/example/javaproject3/week1/day4/VariableEx.java
@@ -0,0 +1,16 @@
+package com.example.javaproject3.week1.day4;
+import com.example.javaproject3.printHello;
+public class VariableEx {
+    public static void main(String[] args) {
+        int iVal;
+        iVal = 0;
+
+        printHello printHello;
+        printHello = new printHello();
+
+        System.out.println(iVal);
+        printHello.print();
+
+
+    }
+}
