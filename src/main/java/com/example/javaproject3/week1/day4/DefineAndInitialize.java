6847141f0db41e805cb428852da8e69595aeaf14
zxfg223 <zxfg223@gmail.com>
2023-04-20 15:27:43 +0900

    feat: 변수 선언 -변수&Scanner

---
+++ src/main/java/com/example/javaproject3/week1/day4/DefineAndInitialize.java
@@ -0,0 +1,15 @@
+package com.example.javaproject3.week1.day4;
+
+import com.example.javaproject3.printHello;
+
+import java.util.Scanner;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        int iVal = 1;
+        printHello printHello = new printHello();
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
