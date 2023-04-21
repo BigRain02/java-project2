afdf2ec801fae43afb673c9c1d606831f81b5ff0
zxfg223 <zxfg223@gmail.com>
2023-04-21 11:46:18 +0900

    feat: 변수 선언 -변수&Scanner

---
+++ src/main/java/com/example/javaproject3/week1/day5/ConstancEx.java
@@ -0,0 +1,16 @@
+package com.example.javaproject3.week1.day5;
+
+import com.example.javaproject3.printHello;
+
+public class ConstancEx {
+    public final int MAX_LEVEL = 3;
+    public static final int MIN_LEVEL = 3;
+    public static void main(String[] args) {
+        final int iVal;
+        iVal = 1;
+        System.out.println(iVal);
+
+        final printHello printHello = new printHello();
+
+    }
+}
---
+++ src/main/java/com/example/javaproject3/week1/day5/SpaceInvader.java
@@ -0,0 +1,12 @@
+package com.example.javaproject3.week1.day5;
+
+public class SpaceInvader {
+    public void LeftMove(){
+        int location = 0;
+        location = -1;
+    }
+    public void RightMove(){
+        int location = 0;
+        location = 1;
+    }
+}
