71e2798b0bf989498941e6c6c51c15e25986f80c
zxfg223 <zxfg223@gmail.com>
2023-04-26 14:31:38 +0900

    feat: 연산자 실습 - 나누기 / 몫

---
+++ src/main/java/com/example/javaproject3/week2/day3/RemainderQuote.java
@@ -0,0 +1,15 @@
+package com.example.javaproject3.week2.day3;
+
+public class RemainderQuote {
+    public static void main(String[] args) {
+        int iVal = 1;
+        System.out.println(iVal / 2);
+
+        int quote = 10 / 3;
+
+        System.out.println("quote = " + quote);
+        
+        int remainder = 10%3;
+        System.out.println("remainder = " + remainder);
+    }
+}
