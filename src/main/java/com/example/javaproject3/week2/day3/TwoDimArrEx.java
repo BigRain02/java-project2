53b14d1f4b1b5b394de9dbcb9d9f9ea934b8b75e
zxfg223 <zxfg223@gmail.com>
2023-04-26 13:33:31 +0900

    feat: 배열 학습 - 2차원 배열 예제

--- src/main/java/com/example/javaproject3/week2/day3/TwoDimArrEx.java
+++ src/main/java/com/example/javaproject3/week2/day3/TwoDimArrEx.java
@@ -9,6 +9,14 @@ public class TwoDimArrEx {
         System.out.println(Arrays.toString(arr[2]));
         System.out.println("------------");
     }
+
+    public static void fillRowOne(int[][] arr , int rowNum){
+        arr[rowNum][0] = 1;
+        arr[rowNum][1] = 1;
+        arr[rowNum][2] = 1;
+        arr[rowNum][3] = 1;
+        arr[rowNum][4] = 1;
+    }
     public static void main(String[] args) {
         int[][] arr = new int[3][3];
 
@@ -20,6 +28,22 @@ public class TwoDimArrEx {
         printArray(arr);
 
         arr[1][1] = 5;
+
+        arr[2][0] = 1; // 2행 0열에 값 할당
+        arr[2][1] = 1; // 2행 1열에 값 할당
+        arr[2][2] = 1; // 2행 2열에 값 할당
+        arr[2][3] = 1; // 2행 3열에 값 할당
+        arr[2][4] = 1; // 2행 4열에 값 할당
+
         printArray(arr);
+
+        arr[0][2] = 1; // 0행 2열에 값 할당
+        arr[1][2] = 1; // 1행 2열에 값 할당
+        arr[2][2] = 1; // 2행 2열에 값 할당
+        arr[3][2] = 1; // 3행 2열에 값 할당
+        arr[4][2] = 1; // 4행 2열에 값 할당
+
+        printArray(arr);
+
     }
 }
---
+++ src/main/java/com/example/javaproject3/week2/day3/TwoDimArrFillRow.java
@@ -0,0 +1,7 @@
+package com.example.javaproject3.week2.day3;
+
+public class TwoDimArrFillRow {
+    public static void main(String[] args) {
+
+    }
+}
