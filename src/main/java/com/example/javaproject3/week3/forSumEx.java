d73a23e260a89bf685d3b4838239a36a3757795d
zxfg223 <zxfg223@gmail.com>
2023-05-02 09:04:25 +0900

    feat: switch case, for loop 문

---
+++ src/main/java/com/example/javaproject3/week3/forSumEx.java
@@ -0,0 +1,15 @@
+package com.example.javaproject3.week3;
+
+import java.util.Scanner;
+
+public class forSumEx
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 0; i <= n; i++){
+            sum += i;
+        }
+    }
+}
