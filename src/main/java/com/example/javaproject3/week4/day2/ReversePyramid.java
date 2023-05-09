f60f4dac49c7bfac97d4d52661506c1a7971d2e3
zxfg223 <zxfg223@gmail.com>
2023-05-09 11:21:10 +0900

    feat: method 학습 - pyramid oop

---
+++ src/main/java/com/example/javaproject3/week4/day2/ReversePyramid.java
@@ -0,0 +1,23 @@
+package com.example.javaproject3.week4.day2;
+
+public class ReversePyramid {
+    private String spaceChar = "0";
+
+    public ReversePyramid(String spaceChar) {
+        this.spaceChar = spaceChar;
+    }
+
+    public String makeALine(int h, int i){
+        return String.format("%s%s\n",spaceChar.repeat(i),"*".repeat(h+3-(i*2)));
+    }
+    public String makeAparallelogramLine(int h, int i){
+        return String.format("%s%s\n",spaceChar.repeat(i),"*".repeat(h));
+    }
+    public static void main(String[] args) {
+        ReversePyramid reversePyramid = new ReversePyramid(" ");
+        int h = 4;
+        for(int i = 0; i < h; i++){
+            System.out.print(reversePyramid.makeALine(h,i));
+        }
+    }
+}
