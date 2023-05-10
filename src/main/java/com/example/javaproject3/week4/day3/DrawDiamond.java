e9a2066df3eb5c0eb834707977f21fe119fe630f
zxfg223 <zxfg223@gmail.com>
2023-05-10 10:21:11 +0900

    feat: 알고리즘 학습 - 다이아몬드

---
+++ src/main/java/com/example/javaproject3/week4/day3/DrawDiamond.java
@@ -0,0 +1,26 @@
+package com.example.javaproject3.week4.day3;
+
+public class DrawDiamond {
+    public static String getRepeatedSymbol(String symbol, int n){
+        return symbol.repeat(n);
+    }
+
+    public static String makeALine(int h, int i) {
+        int pivot = h / 2;
+        if (i <= pivot) {
+            return String.format("%s%s\n", getRepeatedSymbol("0", -2 + h - i - 2), getRepeatedSymbol("*", 2 * i + 1));
+        } else {
+            return String.format("%s%s\n", getRepeatedSymbol("0", i - pivot), getRepeatedSymbol("*", 2 * (h - i) - 1));
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int h = 7;
+        int pivot = h / 2;
+        for(int i = 0; i < h; i++){
+            System.out.print(makeALine(h,i));
+        }
+    }
+}
+
